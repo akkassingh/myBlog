6768ed53debc3852dd10f5bfecc08d9311677062
Akkas Singh <akkassingh@gmail.com>
2023-05-27 21:22:10 -0400

    added comment section and state mgmt

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,42 +1,54 @@
 import React from 'react';
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import axios from 'axios';
-
-import NotFoundPage from "./NotFoundPage";
-import articles from "./article-content";
+import NotFoundPage from './NotFoundPage';
+import CommentsList from '../components/CommentsList';
+import AddCommentForm from '../components/AddCommentForm';
+import articles from './article-content';
 
 const ArticlePage = () => {
-  const [ articleInfo, setArticleInfo ] = useState({
-    upvotes: 0,
-    comments: []
-  })
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+    const { articleId } = useParams();
 
-  useEffect(() => {
-    const loadArticleInfo = async () => {
-      const response  = await axios.get(`/api/articles/${articleId}`);
-      const article = response.data;
-      setArticleInfo(article);  
+    useEffect(() => {
+        const loadArticleInfo = async () => {
+            const response = await axios.get(`/api/articles/${articleId}`);
+            const newArticleInfo = response.data;
+            setArticleInfo(newArticleInfo);
+        }
+
+        loadArticleInfo();
+    }, []);
+
+    const article = articles.find(article => article.name === articleId);
+
+    const addUpvote = async () => {
+        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const updatedArticle = response.data;
+        setArticleInfo(updatedArticle);
     }
-    loadArticleInfo();
-  }, [])
 
-  const { articleId } = useParams();
-  const article = articles.find((article) => article.name === articleId);
+    if (!article) {
+        return <NotFoundPage />
+    }
 
-  if (!article) {
-    return <NotFoundPage />;
-  }
-
-  return (
-    <>
-      <h1>{article.title}</h1>
-      <p>This article has {articleInfo.upvotes} upvote(s)</p>
-      {article.content.map((paragraph, i) => (
-        <p key={i}>{paragraph}</p>
-      ))}
-    </>
-  );
-};
+    return (
+        <>
+        <h1>{article.title}</h1>
+        <div className="upvotes-section">
+            <button onClick={addUpvote}>Upvote</button>
+            <p>This article has {articleInfo.upvotes} upvote(s)</p>
+        </div>
+        {article.content.map((paragraph, i) => (
+            <p key={i}>{paragraph}</p>
+        ))}
+        <AddCommentForm
+            articleName={articleId}
+            onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+        <CommentsList comments={articleInfo.comments} />
+        </>
+    );
+}
 
 export default ArticlePage;
