a6cfa4285f8914388eb475b26b18037d7c5e4269
Akkas Singh <akkassingh@gmail.com>
2023-05-29 00:28:56 -0400

    final changes with hooks and login

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -5,30 +5,36 @@ import axios from 'axios';
 import NotFoundPage from './NotFoundPage';
 import CommentsList from '../components/CommentsList';
 import AddCommentForm from '../components/AddCommentForm';
+import useUser from '../hooks/useUser';
 import articles from './article-content';
 
-import useUser from '../hooks/useUSer';
-
 const ArticlePage = () => {
-    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+    const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [], canUpvote: false });
+    const { canUpvote } = articleInfo;
     const { articleId } = useParams();
 
-    const { user, isloading } = useUser();
+    const { user, isLoading } = useUser();
 
     useEffect(() => {
         const loadArticleInfo = async () => {
-            const response = await axios.get(`/api/articles/${articleId}`);
+            const token = user && await user.getIdToken();
+            const headers = token ? { authtoken: token } : {};
+            const response = await axios.get(`/api/articles/${articleId}`, { headers });
             const newArticleInfo = response.data;
             setArticleInfo(newArticleInfo);
         }
 
-        loadArticleInfo();
-    }, []);
+        if (!isLoading) {
+            loadArticleInfo();
+        }
+    }, [isLoading, user]);
 
     const article = articles.find(article => article.name === articleId);
 
     const addUpvote = async () => {
-        const response = await axios.put(`/api/articles/${articleId}/upvote`);
+        const token = user && await user.getIdToken();
+        const headers = token ? { authtoken: token } : {};
+        const response = await axios.put(`/api/articles/${articleId}/upvote`, null, { headers });
         const updatedArticle = response.data;
         setArticleInfo(updatedArticle);
     }
@@ -42,20 +48,18 @@ const ArticlePage = () => {
         <h1>{article.title}</h1>
         <div className="upvotes-section">
             {user
-            ? <button onClick={addUpvote}>Upvote</button>
-            : <button onClick={addUpvote}>Login to Upvote</button>
-}
+                ? <button onClick={addUpvote}>{canUpvote ? 'Upvote' : 'Already Upvoted'}</button>
+                : <button>Log in to upvote</button>}
             <p>This article has {articleInfo.upvotes} upvote(s)</p>
         </div>
         {article.content.map((paragraph, i) => (
             <p key={i}>{paragraph}</p>
         ))}
-        { user 
-        ? <AddCommentForm
-            articleName={articleId}
-            onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
-            : <button>Login to add a comment</button>
-        }
+        {user
+            ? <AddCommentForm
+                articleName={articleId}
+                onArticleUpdated={updatedArticle => setArticleInfo(updatedArticle)} />
+            : <button>Log in to add a comment</button>}
         <CommentsList comments={articleInfo.comments} />
         </>
     );
